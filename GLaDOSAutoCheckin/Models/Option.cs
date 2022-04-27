1c902d12610a7fa966c43a30e98156f678294bd3
Gaein nidb <mail@gaein.cn>
2022-04-27 12:47:38 +0800

    A option

---
+++ GLaDOSAutoCheckin/Models/Option.cs
@@ -0,0 +1,12 @@
+﻿namespace GLaDOSAutoCheckin.Models;
+
+public class Option
+{
+#nullable disable
+    public string MailAccount { get; set; }
+    public string Password { get; set; }
+    public string BaseUrl { get; set; } = "https://glados.rocks/api/";
+    public ushort MailPort { get; set; } = 143;
+#nullable restore
+    public string? MailHost { get; set; }
+}
