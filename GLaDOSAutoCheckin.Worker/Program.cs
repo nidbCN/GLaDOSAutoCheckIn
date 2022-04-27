54d381b99b6e7c6db21e942871fb964d4a73edc2
Gaein nidb <mail@gaein.cn>
2022-04-27 15:31:07 +0800

    Configure httpClient

--- GLaDOSAutoCheckin.Worker/Program.cs
+++ GLaDOSAutoCheckin.Worker/Program.cs
@@ -1,8 +1,30 @@
+using GLaDOSAutoCheckin.Services;
 using GLaDOSAutoCheckin.Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
+        var baseUrl = context.Configuration["HttpOption:BaseUrl"] ?? "https://glados.rocks/api";
+        services.AddHttpClient<AuthService>(client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+
+            // Add a user-agent default request header.
+            client.DefaultRequestHeaders
+                .UserAgent.ParseAdd(context.Configuration["HttpOption:UserAgent"]);
+            client.DefaultRequestHeaders.Add("authority", "glados.rocks");
+
+            var cookie = context.Configuration["HttpOption:UserAgent"];
+            if (cookie != null)
+            {
+                client.DefaultRequestHeaders.Add("cookie", cookie);
+            }
+        }).ConfigurePrimaryHttpMessageHandler(() =>
+            new HttpClientHandler()
+            {
+                UseCookies = true
+            });
+
         services.AddHostedService<Worker>();
     })
     .Build();
