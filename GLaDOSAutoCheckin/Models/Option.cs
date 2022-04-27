ec3da135ddda735d88c89038c228ebc6c9dde18a
Gaein nidb <mail@gaein.cn>
2022-04-27 12:54:06 +0800

    Configure with command line parser

--- GLaDOSAutoCheckin/Models/Option.cs
+++ GLaDOSAutoCheckin/Models/Option.cs
@@ -1,12 +1,23 @@
-﻿namespace GLaDOSAutoCheckin.Models;
+﻿using CommandLine;
+
+namespace GLaDOSAutoCheckin.Models;
 
 public class Option
 {
 #nullable disable
+    [Value(0)]
     public string MailAccount { get; set; }
+
+    [Value(1)]
     public string Password { get; set; }
+
+    [Option('u', "baseurl")]
     public string BaseUrl { get; set; } = "https://glados.rocks/api/";
+
+    [Option('p', "port")]
     public ushort MailPort { get; set; } = 143;
 #nullable restore
+    
+    [Option('h', "host")]
     public string? MailHost { get; set; }
 }
