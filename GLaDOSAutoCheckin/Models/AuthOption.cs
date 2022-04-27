351e6259f8db8361f0696b60594141c11d10fa34
Gaein nidb <mail@gaein.cn>
2022-04-27 14:12:01 +0800

    margiate to work service

--- GLaDOSAutoCheckin/Models/AuthOption.cs
+++ GLaDOSAutoCheckin/Models/AuthOption.cs
@@ -1,23 +1,16 @@
-﻿using CommandLine;
+﻿namespace GLaDOSAutoCheckin.Models;
 
-namespace GLaDOSAutoCheckin.Models;
-
-public class Option
+public class AuthOption
 {
 #nullable disable
-    [Value(0)]
     public string MailAccount { get; set; }
 
-    [Value(1)]
     public string Password { get; set; }
 
-    [Option('u', "baseurl")]
     public string BaseUrl { get; set; } = "https://glados.rocks/api/";
 
-    [Option('p', "port")]
     public ushort MailPort { get; set; } = 143;
 #nullable restore
-    
-    [Option('h', "host")]
+
     public string? MailHost { get; set; }
 }
---
+++ GLaDOSAutoCheckin/Services/MailService.cs
@@ -0,0 +1,70 @@
+﻿using DnsClient;
+using GLaDOSAutoCheckin.Models;
+using MailKit;
+using MailKit.Net.Imap;
+using GLaDOSAutoCheckin.Utils;
+
+namespace GLaDOSAutoCheckin.Services
+{
+    public class MailService
+    {
+        private readonly ILookupClient _lookupClient;
+        private readonly AuthOption _option;
+
+        public MailService(ILookupClient lookupClient, AuthOption option)
+        {
+            _lookupClient = lookupClient;
+            _option = option;
+
+            if (_option.MailHost is null)
+            {
+                var mailHost = _lookupClient
+                    .Query(_option.MailAccount[(_option.MailAccount.IndexOf('@') + 1)..], QueryType.MX)
+                    .Answers.MxRecords().First().Exchange.Value;
+                _option.MailHost = mailHost;
+            }
+        }
+
+        public string GetAuthCode()
+        {
+            var startTime = DateTime.Now;
+
+            using var client = new ImapClient();
+            client.Connect(_option.MailHost, _option.MailPort, MailKit.Security.SecureSocketOptions.None);
+
+            client.Authenticate(_option.MailAccount, _option.Password);
+
+            // The Inbox folder is always available on all IMAP servers...
+            var inbox = client.Inbox;
+            string? code = null;
+
+            do
+            {
+                inbox.Open(FolderAccess.ReadWrite);
+
+                Console.WriteLine("Total messages: {0}", inbox.Count);
+                Console.WriteLine("Recent messages: {0}", inbox.Recent);
+
+                var authMail = inbox.Last(mail => mail.Subject == "GLaDOS Authentication");
+
+                if (authMail is null)
+                {
+                    Console.WriteLine("Mail not fount, retry...");
+                    break;
+                }
+
+                // if (authMail.Date.DateTime < startTime) break;
+
+                if (AuthCodeUtil.TryGetAuthCodeFromHtml(authMail.HtmlBody, out code))
+                {
+                    Console.WriteLine($"Auth code is {code}");
+                }
+
+            } while (code is null);
+
+            client.Disconnect(true);
+
+            return code;
+        }
+    }
+}
