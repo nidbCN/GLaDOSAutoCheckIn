749b69517e3ba5d72500ed2e19bd58d1d1538b1d
Gaein nidb <mail@gaein.cn>
2022-04-27 12:47:33 +0800

    margite to util static class

---
+++ GLaDOSAutoCheckin/Utils/AuthCodeUtil.cs
@@ -0,0 +1,22 @@
+﻿namespace GLaDOSAutoCheckin.Utils;
+
+public static class AuthCodeUtil
+{
+    public static bool TryGetAuthCodeFromHtml(string html, out string code)
+    {
+        code = string.Empty;
+
+        var startIndex = html.IndexOf("<b>");
+        if (startIndex == -1)
+            return false;
+
+        var half = html[(startIndex + 3)..];
+
+        var endIndex = half.IndexOf("</b>");
+        if (endIndex == -1)
+            return false;
+
+        code = half[..endIndex];
+        return true;
+    }
+}
