ee2ec51561ee72d4d97287f93f7c4521d86073fe
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-05-08 01:02:14 +0800

    Update readme and default timeout

--- GLaDOSAutoCheckin/Models/Options/RequestOption.cs
+++ GLaDOSAutoCheckin/Models/Options/RequestOption.cs
@@ -3,7 +3,7 @@
 public class RequestOption
 {
     public string BaseUrl { get; set; } = "https://glados.rocks/api";
-    public double TimeOut { get; set; } = 600;
+    public double TimeOut { get; set; } = 1200; // 1.2s
     public string UserAgent { get; set; } = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)";
     public string? Cookie { get; set; } = null;
 }
--- README.md
+++ README.md
@@ -52,7 +52,7 @@ dotnet publish -c Release
     "MailHost": "pop3.example.com",
     "UseSSL": false
   },
-  "HttpOption": {
+  "RequestOption": {
     "UserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
     "BaseUrl": "https://glados.rocks/api/",
     "Cookie": null
